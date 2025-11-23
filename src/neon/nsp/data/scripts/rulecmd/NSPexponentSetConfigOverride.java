e5f98649664103b58d26aed06ad07d75c09d1610
NEONN1111 <totallymaplebackup@gmail.com>
2025-11-23 18:35:51 -0500

    Exponent Boss Variant
    Hidden hullmod for Exponent CR.

--- src/neon/nsp/data/scripts/rulecmd/NSPexponentSetConfigOverride.java
+++ src/neon/nsp/data/scripts/rulecmd/NSPexponentSetConfigOverride.java
@@ -27,7 +27,6 @@ public class NSPexponentSetConfigOverride extends BaseCommandPlugin {
 
         MemoryAPI memory = dialog.getInteractionTarget().getMemoryWithoutUpdate();
         memory.set(MemFlags.FLEET_INTERACTION_DIALOG_CONFIG_OVERRIDE_GEN,createConfig());
-Console.showMessage(Global.getSector().getCampaignUI().getCurrentInteractionDialog().getInteractionTarget() instanceof CampaignFleetAPI)
         return true;
     }
 
