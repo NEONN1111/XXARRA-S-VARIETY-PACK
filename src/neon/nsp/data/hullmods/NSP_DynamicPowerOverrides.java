795ca4ea831ff1bed387263321d8236ca404d40c
Vexlia-Artemiss <vexlia.artist@gmail.com>
2026-09-11 17:58:23 +0300

    DPO description changes and Kardakes system bugfix plus refinement

--- src/neon/nsp/data/hullmods/NSP_DynamicPowerOverrides.java
+++ src/neon/nsp/data/hullmods/NSP_DynamicPowerOverrides.java
@@ -10,20 +10,14 @@ import com.fs.starfarer.api.combat.WeaponAPI.WeaponType;
 import com.fs.starfarer.api.ui.Alignment;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
-import neon.nsp.data.shipsystems.AmmoFeedersSubsystemNSP;
 import org.magiclib.subsystems.MagicSubsystemsManager;
 
 import java.awt.Color;
 import java.util.EnumSet;
 
 public class NSP_DynamicPowerOverrides extends BaseHullMod {
-    String MOD_KEY = "NSP_DynamicPowerOverrides";
-
     private static final float RANGE_THRESHOLD = 450.0F;
     private static final float RANGE_MULT = 0.25F;
-
-    float engine_direction = 0.0F;
-    float weapons_direction = 0.0F;
 
     float ENGINE_BONUS_TIME_MAX = 10.0F;
     float ENGINE_BONUS_MAX = 1.0F;
@@ -33,24 +27,17 @@ public class NSP_DynamicPowerOverrides extends BaseHullMod {
     float WEAPONS_BONUS_MAX = 1.0F;
     float WEAPONS_NERF_MAX = 0.5F;
 
-    protected Object STATUSKEY1 = new Object();
-    protected Object STATUSKEY2 = new Object();
-    protected Object STATUSKEY3 = new Object();
-
-    float engine_effect = 0.0F;
-    float weapon_effect = 0.0F;
-
     public NSP_DynamicPowerOverrides() {
     }
 
     public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
-       // stats.getZeroFluxSpeedBoost().modifyMult(id, 0.0F);
+        stats.getZeroFluxSpeedBoost().modifyMult(id, 0.0F);
         stats.getVentRateMult().modifyMult(id, 0.0F);
 
         stats.getWeaponRangeThreshold().modifyFlat(id, RANGE_THRESHOLD);
         stats.getWeaponRangeMultPastThreshold().modifyMult(id, RANGE_MULT);
 
-        stats.getZeroFluxMinimumFluxLevel().modifyFlat(id, 2f); // set to two, meaning boost is always on
+        //stats.getZeroFluxMinimumFluxLevel().modifyFlat(id, 2f); // set to two, meaning boost is always on
         stats.getVentRateMult().modifyMult(id, 0f);
     }
 
@@ -74,20 +61,26 @@ public class NSP_DynamicPowerOverrides extends BaseHullMod {
         float padList = 6.0F;
         float padSig = 1.0F;
 
+
         tooltip.addSectionHeading("Effects", Alignment.MID, 10.0F);
 
         tooltip.addPara("Prevents the use of active venting, drastically reduces weapon ranges past %s units and disables the zero-flux engine boost.",
                 padList, Color.ORANGE, "" + RANGE_THRESHOLD);
 
-        tooltip.addPara("In combat ship actively diverts reactor power to either weapons or engines, depending on what currently used.", padList);
+        tooltip.addPara("Adds %s subsystem,",
+                padList, hColor, "Dynamic Power Overrides");
 
-        tooltip.addPara("In combat, when ship %s, reactor power diverted to weapons providing up to %s firerate and ship's flux dissipation, but reduces maneuverability and max speed by as much as %s.",
-                padList, hColor, "stops using engines", (int)(100.0F + WEAPONS_BONUS_MAX * 100.0F) + "%", (int)(ENGINE_NERF_MAX * 100.0F) + "%");
+        tooltip.addSectionHeading("Dynamic Power Overrides subsystem", Alignment.MID, 10.0F);
 
-        tooltip.addPara("In combat, when ship %s, reactor power diverted to engines providing up to %s maneuverability and max speed, but reduces weapons firerate and ship's flux dissipation by as much as %s.",
-                padList, hColor, "actively using its engines", (int)(100.0F + ENGINE_BONUS_MAX * 100.0F) + "%", (int)(WEAPONS_NERF_MAX * 100.0F) + "%");
+        tooltip.addPara("Subsystem actively diverts reactor power to either weapons or engines, depending on current state.", padList);
 
-        tooltip.addPara("When ship %s, short power surge boost currently active focus.",
+        tooltip.addPara("When %s is active, reactor power diverted to weapons providing up to %s firerate and ship's flux dissipation, but reduces maneuverability and max speed by as much as %s.",
+                padList, hColor, "subsystem", (int)(100.0F + WEAPONS_BONUS_MAX * 100.0F) + "%", (int)(ENGINE_NERF_MAX * 100.0F) + "%");
+
+        tooltip.addPara("When %s is not active, reactor power diverted to engines providing up to %s maneuverability and max speed, but reduces weapons firerate and ship's flux dissipation by as much as %s.",
+                padList, hColor, "subsystem", (int)(100.0F + ENGINE_BONUS_MAX * 100.0F) + "%", (int)(WEAPONS_NERF_MAX * 100.0F) + "%");
+
+        tooltip.addPara("When ship %s, short power surge boosts switched to focus.",
                 padList, hColor, "switches from one focus to another");
     }
 }
--- src/neon/nsp/data/shipsystems/NSP_Kardakesfeeder.java
+++ src/neon/nsp/data/shipsystems/NSP_Kardakesfeeder.java
@@ -5,13 +5,13 @@ import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
 
 public class NSP_Kardakesfeeder extends BaseShipSystemScript {
-
-    public static float EFFECT = 0f;
-    public static final float FLUX_REDUCTION = 50f;
-    public static float RANGE_BONUS = 100f;
-    public static float RECOIL_BONUS = 75f;
+    public static final float FLUX_REDUCTION_MULT = 0.5f;
+    public static float BASE_BONUS = 100f; //Flat buff at activation
+    public static float BONUS_SCALING_CAP = 500f; //Cap to bonus reached when system swithes to ACTIVE state
+    public static float RECOIL_BONUS = 0.25f;
     public static float PROJECTILE_SPEED_BONUS = 50f;
 
+    float scaling_effect = 0f;
 
     @Override
     public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
@@ -20,56 +20,63 @@ public class NSP_Kardakesfeeder extends BaseShipSystemScript {
             return;
         }
 
-        if (state == State.ACTIVE) {
-            EFFECT = EFFECT + 0.02f;
-        }
-
-        float mult = 1f + EFFECT * effectLevel;
-        stats.getBallisticRoFMult().modifyMult(id, mult);
-        stats.getBallisticAmmoRegenMult().modifyMult(id, mult);
-        stats.getBallisticWeaponFluxCostMod().modifyMult(id, 0.5f);
-        stats.getBallisticWeaponRangeBonus().modifyMult(id, mult);
-        stats.getBallisticWeaponRangeBonus().modifyPercent(id, RANGE_BONUS);
-        stats.getEnergyWeaponRangeBonus().modifyPercent(id, RANGE_BONUS);
-        stats.getMaxRecoilMult().modifyMult(id, 1f - (0.01f * RECOIL_BONUS));
-        stats.getRecoilPerShotMult().modifyMult(id, 1f - (0.01f * RECOIL_BONUS));
-        stats.getRecoilDecayMult().modifyMult(id, 1f - (0.01f * RECOIL_BONUS));
-
+        scaling_effect = BASE_BONUS + effectLevel * BONUS_SCALING_CAP;
+        //Ballistic stuff
+        stats.getBallisticWeaponRangeBonus().modifyPercent(id, scaling_effect);
+        stats.getBallisticRoFMult().modifyPercent(id, scaling_effect);
+        stats.getBallisticAmmoRegenMult().modifyPercent(id, scaling_effect);
         stats.getBallisticProjectileSpeedMult().modifyPercent(id, PROJECTILE_SPEED_BONUS);
+        stats.getBallisticWeaponFluxCostMod().modifyMult(id, FLUX_REDUCTION_MULT);
+
+        //Energy stuff
+        stats.getEnergyWeaponRangeBonus().modifyPercent(id, scaling_effect);
+        stats.getEnergyRoFMult().modifyPercent(id, scaling_effect);
+        stats.getEnergyAmmoRegenMult().modifyPercent(id, scaling_effect);
         stats.getEnergyProjectileSpeedMult().modifyPercent(id, PROJECTILE_SPEED_BONUS);
+        stats.getEnergyWeaponFluxCostMod().modifyMult(id, FLUX_REDUCTION_MULT);
 
-
-        stats.getCombatWeaponRepairTimeMult().modifyMult(id, 2 * mult);
-
+        //stats.getCombatWeaponRepairTimeMult().modifyMult(id, 2 * mult); //feels bit extra? also not sure why it was scaling
+        stats.getMaxRecoilMult().modifyMult(id, RECOIL_BONUS);
+        stats.getRecoilPerShotMult().modifyMult(id, RECOIL_BONUS);
+        stats.getRecoilDecayMult().modifyMult(id, RECOIL_BONUS); //Debuffs decay to compensate for recoil reduction buff
     }
 
     @Override
     public void unapply(MutableShipStatsAPI stats, String id) {
-        stats.getBallisticRoFMult().unmodify(id);
-        stats.getCombatWeaponRepairTimeMult().unmodify(id);
-        stats.getBallisticAmmoRegenMult().unmodify(id);
-        stats.getBallisticWeaponFluxCostMod().unmodify(id);
-        stats.getBallisticWeaponRangeBonus().unmodify(id);
-        stats.getBallisticWeaponRangeBonus().unmodify(id);
-        stats.getEnergyWeaponRangeBonus().unmodify(id);
-        stats.getMaxRecoilMult().unmodify(id);
-        stats.getRecoilPerShotMult().unmodify(id);
-        stats.getRecoilDecayMult().unmodify(id);
+        //Ballistic stuff
+        stats.getBallisticWeaponRangeBonus().unmodifyPercent(id);
+        stats.getBallisticRoFMult().unmodifyPercent(id);
+        stats.getBallisticAmmoRegenMult().unmodifyPercent(id);
+        stats.getBallisticProjectileSpeedMult().unmodifyPercent(id);
+        stats.getBallisticWeaponFluxCostMod().unmodifyMult(id);
 
-        stats.getBallisticProjectileSpeedMult().unmodify(id);
-        stats.getEnergyProjectileSpeedMult().unmodify(id);
+        //Energy stuff
+        stats.getEnergyWeaponRangeBonus().unmodifyPercent(id);
+        stats.getEnergyRoFMult().unmodifyPercent(id);
+        stats.getEnergyAmmoRegenMult().unmodifyPercent(id);
+        stats.getEnergyProjectileSpeedMult().unmodifyPercent(id);
+        stats.getEnergyWeaponFluxCostMod().modifyMult(id, FLUX_REDUCTION_MULT);
 
-
-
-        EFFECT = 0;
+        stats.getMaxRecoilMult().modifyMult(id, RECOIL_BONUS);
+        stats.getRecoilPerShotMult().modifyMult(id, RECOIL_BONUS);
+        stats.getRecoilDecayMult().modifyMult(id, RECOIL_BONUS); //Debuffs decay to compensate for recoil reduction buff
     }
 
     @Override
     public StatusData getStatusData(int index, State state, float effectLevel) {
-        float mult = (EFFECT * effectLevel) * 100;
+        scaling_effect = BASE_BONUS + effectLevel * BONUS_SCALING_CAP;
 
-        if (index == 0) {
-            return new StatusData("ballistic ammo regeneration and rate of fire +" + (int) mult + "%", false);
+        if (index == 3) {
+            return new StatusData("weapon range and rate of fire: +" + (int) scaling_effect + "%", false);
+        }
+        if (index == 2) {
+            return new StatusData("weapon flux cost: -" + (int) ((1f-FLUX_REDUCTION_MULT)*100) + "%", false);
+        }
+        if (index == 1) {
+            return new StatusData("weapon recoil: -" + (int) ((1f-RECOIL_BONUS) * 100f) + "%", false);
+        }
+        if (index == 0 && PROJECTILE_SPEED_BONUS > 0) {
+            return new StatusData("projectile speed: +" + (int) PROJECTILE_SPEED_BONUS + "%", false);
         }
         return null;
     }
